1514c518d224931af5f3502d50f705e9fbce30d5
Matt <iammrmateo@gmail.com>
2026-02-17 09:55:52 -0700

    Update to newest released versions and fix sync photos test script.

--- scripts/sync_photos.sh
+++ scripts/sync_photos.sh
@@ -2,6 +2,6 @@
 
 # Activate the virtual environment and sync our photos
 
-source /home/pi/Digital-Photo-Frame/digital-photo-frame/bin/activate
-python /home/pi/Digital-Photo-Frame/scripts/sync_photos.py
+source ~/Digital-Photo-Frame/venv/bin/activate
+python ~/Digital-Photo-Frame/scripts/sync_photos.py
 deactivate
